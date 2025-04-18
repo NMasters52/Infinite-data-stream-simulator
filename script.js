8370f1d58c36665abcd169d0d95845fe4514204f
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-18 15:41:09 +0000

    First custom iterator

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-function* generator() {
-    yield 1
-    yield 2
-    yield 3
+// function* generator() {
+//     yield 1
+//     yield 2
+//     yield 3
+// }
+
+// const obj = generator();
+
+// console.log(obj.next());
+// console.log(obj.next());
+// console.log(obj.next());
+// console.log(obj.next());
+
+class iter {
+    constructor(pageNum, dataSet) {
+        this.pageNum = pageNum;
+        this.dataSet = dataSet;
+    }
+
+    *[Symbol.iterator]() {
+        let startIndex = 0;
+        while(startIndex < this.dataSet.length){
+           let endIndex = Math.min(startIndex + this.pageNum, this.dataSet.length);
+            yield this.dataSet.slice(startIndex, endIndex);
+            startIndex = endIndex;
+        }
+    }
 }
 
-const obj = generator();
+const largeDataSet = Array.from(({length: 100}), (_, index) => `index: ${index + 1}`)
 
-console.log(obj.next());
-console.log(obj.next());
-console.log(obj.next());
-console.log(obj.next());
+const page = new iter(1, largeDataSet);
+
+for (const i of page){
+    console.log(`item = ${i}`, i)
+}
