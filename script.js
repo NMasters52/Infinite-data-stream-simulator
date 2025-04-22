0356660cda0286aa1db90e9810051ad2f58f9016
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-22 15:45:50 +0000

    add custom iterable and iterator using Symbol.iterator

--- script.js
+++ script.js
@@ -11,3 +11,31 @@
 // console.log(obj.next());
 // console.log(obj.next());
 
+const iter = {
+    start: 0,
+    end: 10,
+    step: 2,
+
+    [Symbol.iterator](){
+        let currentValue = this.start;
+        const that = this;
+        return {
+            next(){
+                if(currentValue <= that.end){
+                    const result = {value: currentValue, done:false};
+                     currentValue += that.step;
+                    return result;
+                } else {
+                    return {value: that.end, done: true};
+
+                }
+                
+                
+         }
+        }
+    }
+}
+
+for (const val of iter) {
+    console.log(val);
+}
