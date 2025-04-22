24b33ddcb71e625b7b379b0f0d39273bb2e6f745
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-22 15:31:36 +0000

    restart

--- script.js
+++ script.js
@@ -11,26 +11,3 @@
 // console.log(obj.next());
 // console.log(obj.next());
 
-class iter {
-    constructor(pageNum, dataSet) {
-        this.pageNum = pageNum;
-        this.dataSet = dataSet;
-    }
-
-    *[Symbol.iterator]() {
-        let startIndex = 0;
-        while(startIndex < this.dataSet.length){
-           let endIndex = Math.min(startIndex + this.pageNum, this.dataSet.length);
-            yield this.dataSet.slice(startIndex, endIndex);
-            startIndex = endIndex;
-        }
-    }
-}
-
-const largeDataSet = Array.from(({length: 100}), (_, index) => `index: ${index + 1}`)
-
-const page = new iter(1, largeDataSet);
-
-for (const i of page){
-    console.log(`item = ${i}`, i)
-}
